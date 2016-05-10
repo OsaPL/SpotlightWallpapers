10fbfa2b8bc11611016632ffad006ed99538b735
Osa__PL <osa12pl@gmail.com>
2016-05-10 17:21:14 +0200

    lower cases

--- spotlightextract.bat
+++ spotlightextract.bat
@@ -1,7 +1,7 @@
 :: By Osa__PL, feel free to modify, share. Just give me some credit if you do so. Thanks!!
 :: Copying contents, to a foler
-@ECHO OFF
-MD %CD%\Spotlight\
+@echo off
+md %CD%\Spotlight\
 xcopy %LocalAppData%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets %CD%\Spotlight /E /Y
 cd %CD%\Spotlight\
 ren * *.jpg
@@ -14,19 +14,19 @@ for /r %%F in (*.) do (
         if errorlevel 1 (
             echo Warning: %%F and !name! both exist but are not identical.
         ) else (
-            DEL %%F
+            del %%F
         )
     )
 )
 :: Cleaning for useless app icons, most of them keeps under 80 kb
 for /f  "usebackq delims=;" %%A in (`dir /b *.jpg`) do If %%~zA LSS 81920 del "%%A"
 :: Sorting them using ImageMagick (checks if it is installeds)
-WHERE identify
-IF %ERRORLEVEL% NEQ 0 goto:eof
-MD %CD%\Vertical\
-MD %CD%\Landscape\
-FOR /F "tokens=1-3 delims=+" %%A in ('identify -format "%%w+%%h+%%f\r\n" *.jpg') DO (
-    ECHO %%A,%%B,%%C
-    IF %%A GEQ %%B move %cd%\%%C %cd%\Landscape
-    IF %%A LEQ %%B move %cd%\%%C %cd%\Vertical
+where identify
+if %ERRORLEVEL% NEQ 0 goto:eof
+md %CD%\Vertical\
+md %CD%\Landscape\
+for /f "tokens=1-3 delims=+" %%A in ('identify -format "%%w+%%h+%%f\r\n" *.jpg') do (
+    do %%A,%%B,%%C
+    if %%A GEQ %%B move %cd%\%%C %cd%\Landscape
+    if %%A LEQ %%B move %cd%\%%C %cd%\Vertical
 )
